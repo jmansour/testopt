b69da9101f78364efe69ae97294acd9d3668d456
john mansour <mansourjohn@gmail.com>
2012-02-26 17:27:37 +1100

    added some stuff.. i think

---
+++ SimpleFlowDenseOF.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "cv.h"
+#include "highgui.h"
+
+using namespace std;
+using namespace cv;
+
+#include "SimpleFlowDenseOF.h"
+
+bool SimpleFlowDenseOF::calcFlow( Mat &resultImage, Mat *resultImageScale ){
+   return 0;
+}
--- test.cpp
+++ test.cpp
@@ -15,21 +15,14 @@ int main( int argc, char** argv )
       cout << "You must provide at least two images! Fool!" << endl;
       exit(1);
    }
-   char* inputImageNames[argc-1];
-   for (int ii=0; ii<argc-1; ii++)
-      inputImageNames[ii] = argv[ii+1];
 
    vector<Mat> inputImages;
-   for (int ii=0; ii<argc-1; ii++)
-      inputImages.push_back( imread(inputImageNames[ii]) );
+   for (unsigned ii=0; ii<numImages; ii++)
+      inputImages.push_back( imread(string(argv[ii])) );
 
-   SimpleFlowDenseOF testEmptyOF;
-   testEmptyOF.setImages( inputImages );
+   SimpleFlowDenseOF simpleFlow;;
+   simpleFlow.setImages( inputImages );
 
-   vector<Mat> &inputImages2 = testEmptyOF.getImages();
-   
-   cout << "the number of images is " << testEmptyOF.getNumImages() << endl;
-   cout << "the number of images is (var2) " << inputImages2.size() << endl;
    return 0;
 }
    
